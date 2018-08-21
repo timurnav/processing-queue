deb928e5655db65fc56b41922ce77bf75b11d88d
timurnav <timurnav@yandex-team.ru>
2018-08-22 00:08:28 +0300

    minimum viable product

---
+++ src/com/developer/timurnav/MainClass.java
@@ -0,0 +1,61 @@
+package com.developer.timurnav;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+
+public class MainClass {
+
+    public static void main(String[] args) throws Exception {
+        CountDownLatch startLatch = new CountDownLatch(1);
+        CountDownLatch finishLatch = new CountDownLatch(23);
+
+        SequentialEventsExecutor executor = new SequentialEventsExecutor();
+        LocalDateTime now = LocalDateTime.now();
+        executor.start();
+
+        register(executor, now, startLatch, finishLatch, 15, 6000);
+        register(executor, now, startLatch, finishLatch, 14, 5500);
+        register(executor, now, startLatch, finishLatch, 9, 5000);
+        register(executor, now, startLatch, finishLatch, 1, 1000);
+        register(executor, now, startLatch, finishLatch, 15, 6000);
+        register(executor, now, startLatch, finishLatch, 5, 2400);
+        register(executor, now, startLatch, finishLatch, 6, 2500);
+        register(executor, now, startLatch, finishLatch, 7, 3000);
+        register(executor, now, startLatch, finishLatch, 13, 5400);
+        register(executor, now, startLatch, finishLatch, 15, 6000);
+        register(executor, now, startLatch, finishLatch, 8, 4000);
+        register(executor, now, startLatch, finishLatch, 15, 6000);
+        register(executor, now, startLatch, finishLatch, 11, 5200);
+        register(executor, now, startLatch, finishLatch, 2, 2100);
+        register(executor, now, startLatch, finishLatch, 7, 3000);
+        register(executor, now, startLatch, finishLatch, 15, 6000);
+        register(executor, now, startLatch, finishLatch, 3, 2200);
+        register(executor, now, startLatch, finishLatch, 15, 6000);
+        register(executor, now, startLatch, finishLatch, 10, 5100);
+        register(executor, now, startLatch, finishLatch, 12, 5300);
+        register(executor, now, startLatch, finishLatch, 1, 1000);
+        register(executor, now, startLatch, finishLatch, 4, 2300);
+        register(executor, now, startLatch, finishLatch, -1, -1000);
+
+        startLatch.countDown();
+        finishLatch.await();
+        executor.interrupt();
+    }
+
+    private static void register(SequentialEventsExecutor executor, LocalDateTime now, CountDownLatch startLatch, CountDownLatch finishLatch, int number, int delayMillis) {
+        CompletableFuture.runAsync(() -> {
+            try {
+                LocalDateTime executionTime = now.plus(delayMillis, ChronoUnit.MILLIS);
+                startLatch.await();
+                executor.submit(executionTime, () -> {
+                    System.out.println(number);
+                    finishLatch.countDown();
+                });
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
---
+++ src/com/developer/timurnav/SequentialEventsExecutor.java
@@ -0,0 +1,68 @@
+package com.developer.timurnav;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.PriorityQueue;
+
+public class SequentialEventsExecutor extends Thread {
+
+    private final PriorityQueue<ClockTask> queue = new PriorityQueue<>();
+
+    public void submit(LocalDateTime executionTime, Runnable event) {
+        Objects.requireNonNull(executionTime, "Execution time must be not null");
+        Objects.requireNonNull(event, "Event must be not null");
+        synchronized (queue) {
+            queue.add(new ClockTask(executionTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(), event));
+            queue.notify();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                getEvent().run(); //todo run async
+            } catch (InterruptedException e) {
+                return; //todo do something with tasks in queue
+            } catch (Exception e) {
+                e.printStackTrace(); //todo process exception properly
+            }
+        }
+    }
+
+    private Runnable getEvent() throws InterruptedException {
+        synchronized (queue) {
+            while (true) {
+                long timeToWait = Optional.ofNullable(queue.peek())
+                    .map(task -> task.executionTime - System.currentTimeMillis())
+                    .orElse(Long.MAX_VALUE);
+                if (timeToWait <= 0) {
+                    break;
+                }
+                queue.wait(timeToWait);
+            }
+            return Objects.requireNonNull(queue.poll(), "something went wrong").event;
+        }
+    }
+
+    private class ClockTask implements Comparable<ClockTask> {
+
+        private final long executionTime;
+        private final Runnable event;
+
+        private ClockTask(long executionTime, Runnable event) {
+            this.executionTime = executionTime;
+            this.event = event;
+        }
+
+        @Override
+        public int compareTo(ClockTask o) {
+            if (this.executionTime == o.executionTime) {
+                return 0;
+            }
+            return this.executionTime > o.executionTime ? 1 : -1;
+        }
+    }
+}
